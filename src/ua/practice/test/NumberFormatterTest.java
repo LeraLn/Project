2ef281d0697a111c3f247c68537be657db2057d8
[Your <rjflklvx@email.com>
2021-07-26 17:28:05 +0300

    first commit

---
+++ src/ua/practice/test/NumberFormatterTest.java
@@ -0,0 +1,43 @@
+package ua.practice.test;
+
+import org.junit.*;
+import org.junit.Test;
+import ua.practice.project.NumberFormatter;
+
+public class NumberFormatterTest {
+String string;
+NumberFormatter numFor=new NumberFormatter();
+
+
+@Test
+    public void TestAllMethod(){
+    string="-764";
+    int expected=-764;
+    Assert.assertEquals(expected, numFor.parseInt(string)); }
+
+    @Test
+    public void TestMethod(){
+        string="764";
+        int expected=764;
+        Assert.assertEquals(expected, numFor.convertStringInput(string)); }
+
+
+    @Test
+    public void TestingMinus(){
+        string="-764";
+        int expected=-764;
+        Assert.assertEquals(expected, numFor.convertStringInputWithMinus(string)); }
+
+    @Test
+    public void TestingPlus(){
+        string="+764";
+        int expected=764;
+        Assert.assertEquals(expected, numFor.convertStringInputWithPlus(string)); }
+    @Test
+    public void TestingZero(){
+        string="10000";
+        char[]chars=string.toCharArray();
+        int expected=10000;
+        Assert.assertEquals(expected,numFor.getQuantityOfZero(chars));
+         }
+}
